bd1fca652cc408fc0d561f2f5b1d44174a88a002
Erin Orstrom <eorstrom2010@gmail.com>
2017-09-10 15:04:12 -0500

    Added Comic Books list view

--- comic-book-gallery/Data/ComicBookRepository.cs
+++ comic-book-gallery/Data/ComicBookRepository.cs
@@ -56,6 +56,11 @@ namespace comicbookgallery.Data
         	}
         };
 
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
